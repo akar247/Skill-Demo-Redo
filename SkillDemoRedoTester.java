4cb884fc9e430a66673c13e1323b7c2dd16de33a
akar247 <akar@ucsd.edu>
2022-05-10 22:48:21 -0700

    Fixed Tester File

--- SkillDemoRedoTester.java
+++ SkillDemoRedoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoRedoTester {
     @Test
     public void testProduct(){
-        assertEquals(2, SkillDemoRedo.product(1, 1));
+        assertEquals(2, SkillDemoRedo.product(2, 1));
     }
 }
